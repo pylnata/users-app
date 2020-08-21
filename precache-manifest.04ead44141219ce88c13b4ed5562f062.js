f6a95adaa67bdfca7cb306a7773b2a25e027ba3b
pylnata <natalia.pylypenko@hubplanner.com>
2020-08-21 19:03:09 +0200

    Updates

---
+++ precache-manifest.04ead44141219ce88c13b4ed5562f062.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be176c98e61603bba5652425ddc63baa",
+    "url": "/users-app/index.html"
+  },
+  {
+    "revision": "24f1f508927df32d8a83",
+    "url": "/users-app/static/css/3.265a7ead.chunk.css"
+  },
+  {
+    "revision": "8572faf32b4e400bf330",
+    "url": "/users-app/static/css/4.55f8adac.chunk.css"
+  },
+  {
+    "revision": "caed28920d37756b61c6",
+    "url": "/users-app/static/css/main.529ef8d7.chunk.css"
+  },
+  {
+    "revision": "d564c10dfef64e97b918",
+    "url": "/users-app/static/js/2.a3274fb0.chunk.js"
+  },
+  {
+    "revision": "3780c1cb5da1f1b68e5488f5a00d0317",
+    "url": "/users-app/static/js/2.a3274fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f1f508927df32d8a83",
+    "url": "/users-app/static/js/3.a3edb7b0.chunk.js"
+  },
+  {
+    "revision": "8572faf32b4e400bf330",
+    "url": "/users-app/static/js/4.e34044fd.chunk.js"
+  },
+  {
+    "revision": "31cdbc72c6731525298f",
+    "url": "/users-app/static/js/5.631c9b5c.chunk.js"
+  },
+  {
+    "revision": "caed28920d37756b61c6",
+    "url": "/users-app/static/js/main.326c175a.chunk.js"
+  },
+  {
+    "revision": "da92dc96f2952ef114ad",
+    "url": "/users-app/static/js/runtime-main.a67f4b7d.js"
+  }
+]);
